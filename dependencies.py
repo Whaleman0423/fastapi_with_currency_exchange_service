c503f6ea77e5dfaae0854fa5e216f8c072322d3c
unknown <sheiyuray@gmail.com>
2024-06-21 04:15:23 +0800

    Add dependencies for service injection

---
+++ dependencies.py
@@ -0,0 +1,27 @@
+from services.currency_exchange_service import CurrencyExchangeService
+
+
+# 匯率資料
+exchange_rates = {
+    "currencies": {
+        "TWD": {
+            "TWD": 1,
+            "JPY": 3.669,
+            "USD": 0.03281
+        },
+        "JPY": {
+            "TWD": 0.26956,
+            "JPY": 1,
+            "USD": 0.00885
+        },
+        "USD": {
+            "TWD": 30.444,
+            "JPY": 111.801,
+            "USD": 1
+        }
+    }
+}
+
+# 依賴注入匯率服務
+def get_exchange_service():
+    return CurrencyExchangeService(exchange_rates["currencies"])
